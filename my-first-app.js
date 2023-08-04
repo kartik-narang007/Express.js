17b8feb2af47b147f99b6a4e088206e4911db45c
Kartik Narang <narangk007@gmail.com>
2023-08-04 17:10:47 +0530

    Express Router, 404 Page,Filtering Routes

--- my-first-app.js
+++ my-first-app.js
@@ -2,23 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST">' +
-    '<input type="text" name="title" placeholder="Product Title">' +
-    '<input type="text" name="size" placeholder="Product Size">' +
-    '<button type="submit">Add product</button></form>');
-});
+app.use('/admin',adminRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log('Product Title:', req.body.title);
-  console.log('Product Size:', req.body.size);
-  res.redirect('/');
-});
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express.js</h1>');
-});
+app.use((req, res, next) =>{
+  res.status(404).send('<h1>Page not found!</h1>')
+})
 
 app.listen(3000);
